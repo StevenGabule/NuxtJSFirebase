ad0fe5679ef35211c0d101a3643180d3d6b36572
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-01-30 11:03:51 +0800

    implement sending comments and liking user comments with sorting

--- pages/headlines/_slug.vue
+++ pages/headlines/_slug.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="md-layout md-alignment-center" style="margin: 5em 0">
+  <div class="md-layout md-alignment-center" style="margin: 5em 0">
+    <div class="md-layout-item md-size-75 md-small-size-80 md-xsmall-size-100">
+
       <!-- headline Markup -->
-      <md-card class="md-layout-item md-size-75 md-small-size-80 md-xsmall-size-100">
+      <md-card>
         <md-card-media style="height: 300px;" md-ratio="16:9">
-          <img :src="headline.urlToImage" :alt="headline.title" />
+          <img :src="headline.urlToImage" :alt="headline.title"/>
         </md-card-media>
         <md-card-header>
           <div class="md-title">
@@ -13,7 +15,7 @@
             {{headline.source.name}}
             <md-icon>book</md-icon>
           </div>
-          <div class="md-subhead">
+          <div class="md-subhead" v-if="headline.author">
             {{ headline.author}}
             <md-icon>face</md-icon>
           </div>
@@ -22,21 +24,85 @@
           {{headline.content}}
         </md-card-content>
       </md-card>
+      <!-- comment form area -->
+      <form @submit.prevent="sendComment">
+        <md-field>
+          <label>Enter your comment</label>
+          <md-textarea v-model="text" :disabled="loading || !user"></md-textarea>
+          <md-icon>description</md-icon>
+        </md-field>
+        <md-button class="md-primary md-raised" type="submit" :disabled="loading || !user">Send comment</md-button>
+      </form>
+
+      <!-- comments -->
+      <md-list class="md-triple-line" style="margin-top: 1em">
+        <md-list-item v-for="comment in headline.comments" :key="comment.id">
+          <md-avatar>
+            <img :src="comment.user.avatar" :alt="comment.user.username">
+          </md-avatar>
+          <div class="md-list-item-text">
+            <span>{{comment.user.username}}</span>
+            <span>{{comment.publishedAt}}</span>
+            <p>{{comment.text}}</p>
+          </div>
+
+          <md-badge class="md-primary" md-position="bottom" :md-content="comment.likes" />
+            <md-button @click="likeComment(comment.id)" class="md-icon-button" :disabled="loading || !user">
+              <md-icon>thumb_up</md-icon>
+            </md-button>
+        </md-list-item>
+      </md-list>
+
       <!-- back button -->
       <md-button class="md-fixed md-primary md-fab md-fab-bottom-right" @click="$router.go(-1)">
         <md-icon>arrow_back</md-icon>
       </md-button>
+
     </div>
+  </div>
 </template>
 
 <script>
+    import uuidv4 from 'uuid/v4'
+
     export default {
+        data: () => ({
+            text: ''
+        }),
+
         async fetch({store, params}) {
             await store.dispatch('loadHeadline', params.slug);
         },
         computed: {
             headline() {
                 return this.$store.getters.headline;
+            },
+            loading() {
+                return this.$store.getters.loading;
+            },
+            user() {
+                return this.$store.getters.user;
+            }
+        },
+        methods: {
+            async sendComment() {
+                const comment = {
+                    id: uuidv4(),
+                    text: this.text,
+                    user: this.getCommentUserData(),
+                    publishedAt: Date.now(),
+                    likes: 0
+                };
+                await this.$store.dispatch('sendComment', comment);
+                this.text = '';
+            },
+            getCommentUserData() {
+                const commentUserData = {...this.user};
+                commentUserData['username'] = commentUserData['email'].split('@')[0];
+                return commentUserData;
+            },
+            async likeComment(commentId) {
+                await this.$store.dispatch('likeComment', commentId);
             }
         }
     }
--- store/index.js
+++ store/index.js
@@ -69,12 +69,12 @@ const createStore = () => {
         commit('setHeadlines', headlines);
       },
 
-      async addHeadlineToFeed({ state }, headline) {
+      async addHeadlineToFeed({state}, headline) {
         const feedRef = db.collection(`users/${state.user.email}/feed`).doc(headline.title);
         await feedRef.set(headline);
       },
 
-      async loadUserFeed({ state, commit }) {
+      async loadUserFeed({state, commit}) {
         if (state.user) {
           const feedRef = db.collection(`users/${state.user.email}/feed`);
           await feedRef.onSnapshot(querySnapshot => {
@@ -91,14 +91,67 @@ const createStore = () => {
         }
       },
 
+      async likeComment({state, commit}, commentId) {
+        const commentsRef = db.collection(`headlines/${state.headline.slug}/comments`).orderBy('likes', 'desc' );
+        const likedCommentRef = db.collection('headlines').doc(state.headline.slug).collection('comments').doc(commentId);
+        await likedCommentRef.get().then(doc => {
+          if (doc.exists) {
+            const prevLikes = doc.data().likes;
+            const currentLikes = prevLikes + 1;
+            likedCommentRef.update({
+              likes: currentLikes
+            });
+          }
+        });
+        await commentsRef.onSnapshot(querySnapshot => {
+          let loadedComments = [];
+          querySnapshot.forEach(doc => {
+            loadedComments.push(doc.data());
+            const updatedHeadline = {
+              ...state.headline,
+              comments: loadedComments
+            };
+            commit('setHeadline', updatedHeadline);
+          })
+        });
+      },
+
       async loadHeadline({commit}, headlineSlug) {
         const headlineRef = db.collection('headlines').doc(headlineSlug);
-        await headlineRef.get().then(doc => {
+        const commentsRef = db.collection(`headlines/${headlineSlug}/comments`).orderBy('likes', 'desc');
+
+        let loadedHeadline = {};
+        await headlineRef.get().then(async doc => {
           if (doc.exists) {
-            const headline = doc.data();
-            commit('setHeadline', headline)
+            loadedHeadline = doc.data();
+            await commentsRef.get().then(querySnapshot => {
+              if (querySnapshot.empty) {
+                commit('setHeadline', loadedHeadline);
+              }
+              let loadedComments = [];
+              querySnapshot.forEach(doc => {
+                loadedComments.push(doc.data());
+                loadedHeadline['comments'] = loadedComments;
+                commit('setHeadline', loadedHeadline);
+              })
+            })
           }
         })
+      },
+
+      async sendComment({state, commit}, comment) {
+        const commentsRef = db.collection(`headlines/${state.headline.slug}/comments`);
+        commit('setLoading', true);
+        await commentsRef.doc(comment.id).set(comment);
+        await commentsRef.orderBy('likes', 'desc').get().then(querySnapShot => {
+          let comments = [];
+          querySnapShot.forEach(doc => {
+            comments.push(doc.data());
+            const updatedHeadline = {...state.headline, comments};
+            commit('setHeadline', updatedHeadline);
+          })
+        });
+        commit('setLoading', false);
       },
 
       async saveHeadline(context, headline) {
@@ -131,12 +184,12 @@ const createStore = () => {
           let user;
           if (userPayload.action === "register") {
             const avatar = `http://gravatar.com/avatar/${md5(authUserData.email)}?d=identicon`;
-             user = {email: authUserData.email, avatar};
+            user = {email: authUserData.email, avatar};
             await db.collection('users').doc(userPayload.email).set(user);
           } else {
             const loginRef = db.collection('users').doc(userPayload.email);
-            const loggedInUser=await loginRef.get();
-            user  = loggedInUser.data();
+            const loggedInUser = await loginRef.get();
+            user = loggedInUser.data();
           }
           commit("setUser", user);
           commit('setToken', authUserData.idToken);
